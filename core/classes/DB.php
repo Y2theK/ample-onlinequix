d66c1d8435b98c5d0637201470ae59a89870770f
Y2K <yeyintkyaw300@gmail.com>
2021-10-01 13:15:48 -0800

    ample-online-version-1.01

--- core/classes/DB.php
+++ core/classes/DB.php
@@ -5,7 +5,8 @@ class DB
     private static $res,$data,$count,$sql;
     public function __construct()
     {
-        self::$dbh = new PDO("mysql:host=localhost;dbname=online_quiz_system","root","");
+        // self::$dbh = new PDO("mysql:host=localhost;dbname=online_quiz_system","root","");
+        self::$dbh = new PDO("mysql:host=remotemysql.com;dbname=yfrmNVVGdc","yfrmNVVGdc","IqMXPOkjzo");
         self::$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     }
     public function query($params = [])
